34adea2ab4d204a00ab01f363112735178af0c90
Theo Mathieu <tmathieu.github@fastmail.com>
2023-08-21 12:55:37 +0200

    fix: release

--- dist/index.js
+++ dist/index.js
@@ -9794,9 +9794,9 @@ const github = __nccwpck_require__(7811);
 
 try {
     const branch = core.getInput('branch');
-    let manifest = "templates-default"
+    let manifest = "application.yaml"
     if (branch.indexOf("release/") != -1 || branch.indexOf("release-") != -1 || branch.indexOf("develop") != -1) {
-        manifest = "templates-stable"
+        manifest = "application-stable.yaml"
     }
     core.setOutput("manifest", manifest);
     console.log(`The manifest: ${manifest}`);
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const github = require('@actions/github');
 
 try {
     const branch = core.getInput('branch');
-    let manifest = "templates-default"
+    let manifest = "application.yaml"
     if (branch.indexOf("release/") != -1 || branch.indexOf("release-") != -1 || branch.indexOf("develop") != -1) {
-        manifest = "templates-stable"
+        manifest = "application-stable.yaml"
     }
     core.setOutput("manifest", manifest);
     console.log(`The manifest: ${manifest}`);
